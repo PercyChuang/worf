1c55f910a17919e9e4c57d086cabefd07c45b45f
BuddhaZhuang <592119990@qq.com>
2017-06-06 13:50:12 +0800

    ee

--- worf-datasource/src/main/java/orj/worf/datasource/PropertyPlaceholderConfigurerCipher.java
+++ worf-datasource/src/main/java/orj/worf/datasource/PropertyPlaceholderConfigurerCipher.java
@@ -1,12 +1,10 @@
 package orj.worf.datasource;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.Properties;
-
-import javax.management.RuntimeErrorException;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,84 +12,36 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.FatalBeanException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.util.StringUtils;
 
 public class PropertyPlaceholderConfigurerCipher  extends PropertyPlaceholderConfigurer {
 	private static final Logger logger = LoggerFactory.getLogger(PropertyPlaceholderConfigurerCipher.class);
-	private static List<String> keys = new ArrayList<String>(Arrays.asList(
-			"jdbc.driverclass",
-			"jdbc1.driverclass", 
-			"jdbc2.driverclass",
-			"jdbc3.driverclass", 
-			"jdbc4.driverclass",
-			
-			"jdbc.url",
-			"jdbc1.url",
-			"jdbc2.url",
-			"jdbc3.url",
-			"jdbc4.url",
-			
-			"jdbc.username",
-			"jdbc1.username",
-			"jdbc2.username",
-			"jdbc3.username",
-			"jdbc4.username",
-			
-			"jdbc.password",
-			"jdbc1.password",
-			"jdbc2.password",
-			"jdbc3.password",
-			"jdbc4.password",
-			
-			"jdbc.pool.size.max",
-			"jdbc1.pool.size.max",
-			"jdbc2.pool.size.max",
-			"jdbc3.pool.size.max",
-			"jdbc4.pool.size.max"
-			));
 	
 	@Override
 	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
 			throws BeansException {
 		try{
-			for (String key : keys) {
-				String value = props.getProperty(key);
-				if (null == value || value.length()==0) {
-					value = props.getProperty(getDefaultKey(key));
-					props.setProperty(key, value);
-				}else{
-					String decryptData = RJSCipher.decryptData(value);
-					props.setProperty(key, decryptData);
+			Enumeration<Object> keys = props.keys();
+			while (keys.hasMoreElements()) {
+				String key = (String)keys.nextElement();
+				key = key.trim();
+				logger.info("get prop key:{}",key);
+				if(key.startsWith("pwd_")) {
+					String pwdValue = props.getProperty(key);
+					if (pwdValue != null) {
+						pwdValue = pwdValue.trim();
+						if (!"".equals(pwdValue)) {
+							String decryptData = RJSCipher.decryptData(pwdValue);
+							logger.info("the key is startwith pwd_. get the decrypt data :{}",decryptData);
+							key = key.substring(4);
+							props.setProperty(key, decryptData);
+						}
+					}
 				}
 			}
 		} catch (Exception e) {
-			logger.error("无法解密数据库文件。数据库文件必须要加密存放!",e);
-			throw new FatalBeanException("无法解密数据库文件。数据库文件必须要加密存放!");
+			logger.error("配置文件设置异常",e);
+			throw new FatalBeanException("配置文件配置异常!");
 		}
 		super.processProperties(beanFactoryToProcess, props);
-	}
-	
-	public String getDefaultKey(String nullValueKey) {
-		 if (nullValueKey.endsWith(".driverclass")){
-			 logger.warn(nullValueKey+"值为null，采用默认jdbc.driverclass配置!");
-			 return "jdbc.driverclass";
-		 }
-		 if (nullValueKey.endsWith(".url")){
-			 logger.warn(nullValueKey+"值为null，采用默认jdbc.url配置!");
-			 return "jdbc.url";
-		 }
-		 if (nullValueKey.endsWith(".username")){
-			 logger.warn(nullValueKey+"值为null，采用默认jdbc.username配置!");
-			 return "jdbc.username";
-		 }
-		 if (nullValueKey.endsWith(".password")){
-			 logger.warn(nullValueKey+"值为null，采用默认jdbc.password配置!");
-			 return "jdbc.password";
-		 }
-		 if (nullValueKey.endsWith(".pool.size.max")){
-			 logger.warn(nullValueKey+"值为null，采用默认jdbc.pool.size.max配置!");
-			 return "jdbc.pool.size.max";
-		 }
-		 return  null;
 	}
 }
